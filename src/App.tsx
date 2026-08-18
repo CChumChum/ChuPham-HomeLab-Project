25e5e4be5fe528c57d0275b3fd8f984974df8b37
An Hoang Chu <cchum900@gmail.com>
2026-08-18 22:21:28 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-zinc-950 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">ChuPham HomeLab</h1>
+    </main>
+  );
+}
+
+export default App;
